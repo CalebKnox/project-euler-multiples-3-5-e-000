247fc05456802ee9d5f261deef9a2cde5f45e161
CalebKnox <knoxcaleb@aol.com>
2015-11-30 20:05:13 +0000

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,24 @@
-# Enter your procedural solution here!
+def collect_multiples(limit)
+  collected_multiples = []
+  limit.times do |num|
+    if num % 3 == 0 && num != 0
+      collected_multiples.push(num)
+    elsif num % 5 == 0 && num != 0
+      collected_multiples.push(num)
+    end
+  end
+    return collected_multiples
+end
+
+def sum_multiples(limit)
+  sum = 0
+  collected_multiples = collect_multiples(limit)
+  collected_multiples.each do |num|
+    sum += num
+  end
+  return sum
+end
+
+
+
+
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,26 @@
 # Enter your object-oriented solution here!
+class Multiples
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def collect_multiples
+    collected_multiples = []
+    @limit.times do |num|
+    if num % 3 == 0 && num != 0
+      collected_multiples.push(num)
+    elsif num % 5 == 0 && num != 0
+      collected_multiples.push(num)
+    end
+    end
+    return collected_multiples
+  end
+
+  def sum_multiples
+    sum = 0
+    collect_multiples.each do |num|
+      sum += num
+    end
+    return sum
+    end
+end
